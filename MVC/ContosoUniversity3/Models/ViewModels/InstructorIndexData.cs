508485c614d4b959ba31d0f41ee3bb7a62d85648
zheka <zheka@HOME-PC>
2025-10-17 21:28:27 +0300

    Courses Tought by Selected Instructor

---
+++ MVC/ContosoUniversity3/Models/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,11 @@
+﻿using ContosoUniversity3.Models;
+
+namespace ContosoUniversity3.Models.ViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor>Instructors { get; set; }
+        public IEnumerable<Course>Courses { get; set; }
+        public IEnumerable<Enrollment>Enrollments { get; set; }
+    }
+}
